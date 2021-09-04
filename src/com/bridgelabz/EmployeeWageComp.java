bfa92c4dbd9487a08b695258866c0d56c9357a0b
RajendraGund <rajendragund2015@gmail.com>
2021-09-04 07:20:18 +0530

    Modified : the message for user and add .gitignore file

--- src/com/bridgelabz/EmployeeWageComp.java
+++ src/com/bridgelabz/EmployeeWageComp.java
@@ -3,5 +3,6 @@ package com.bridgelabz;
 public class EmployeeWageComp {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee wage program !!");
+        System.out.println("This is a Employee Wage Program");
     }
 }
